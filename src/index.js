e131472e96ec17fc6f99c5e64fc90ec1caed9978
fherrerao <fherrera0206@gmail.com>
2022-01-26 17:11:36 -0500

    Create new game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './style.css';
+import "./style.css";
+
+const url =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
+
+const createNewGame = async () => {
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Fernando's game",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  const data = await response.json();
+  console.log(data.result.slice(14, 34));
+};
+
+createNewGame();
