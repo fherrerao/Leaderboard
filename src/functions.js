fbc90a6b11035571191c77f538be737a2c34ecf8
fherrerao <fherrera0206@gmail.com>
2022-01-27 12:00:52 -0500

    Fix linter errors

--- src/functions.js
+++ src/functions.js
@@ -1,22 +1,22 @@
-const inputName = document.querySelector("#input-name");
-const inputScore = document.querySelector("#input-score");
-const playerList = document.querySelector(".leaderboard");
-const messageParagraph = document.querySelector(".form");
+const inputName = document.querySelector('#input-name');
+const inputScore = document.querySelector('#input-score');
+const playerList = document.querySelector('.leaderboard');
+const messageParagraph = document.querySelector('.form');
 
 export default class Players {
   static url =
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
-  static idGame = "";
+  static idGame = '';
 
   static createNewGame = async () => {
     const response = await fetch(this.url, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         name: "Fernando's game",
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
     });
     const data = await response.json();
@@ -24,24 +24,23 @@ export default class Players {
   };
 
   static addNewPlayer = async () => {
-    const url =
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0FZjRBVl8F9k1xZJg0ir/scores/";
-    if (inputName.value === "" || inputScore.value === "") {
+    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0FZjRBVl8F9k1xZJg0ir/scores/';
+    if (inputName.value === '' || inputScore.value === '') {
       return;
     }
     const response = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         user: inputName.value,
         score: parseInt(inputScore.value, 10),
       }),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
     });
 
     const data = await response.json();
-    const message = document.createElement("p");
+    const message = document.createElement('p');
     message.innerHTML = data.result;
     messageParagraph.appendChild(message);
     setTimeout(() => {
@@ -50,20 +49,19 @@ export default class Players {
   };
 
   static getPlayer = async () => {
-    const url =
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0FZjRBVl8F9k1xZJg0ir/scores/";
+    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0FZjRBVl8F9k1xZJg0ir/scores/';
     const response = await fetch(url);
     const data = await response.json();
-    playerList.innerHTML = "";
+    playerList.innerHTML = '';
     if (data.result.length === 0) {
-      const players = document.createElement("li");
-      players.classList.add("scores");
-      players.innerHTML = "Empty list, please add a new score";
+      const players = document.createElement('li');
+      players.classList.add('scores');
+      players.innerHTML = 'Empty list, please add a new score';
       playerList.appendChild(players);
     }
     data.result.forEach((element) => {
-      const players = document.createElement("li");
-      players.classList.add("scores");
+      const players = document.createElement('li');
+      players.classList.add('scores');
       players.innerHTML = `${element.user}: ${element.score}`;
       playerList.appendChild(players);
     });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "./style.css";
-import Players from "./functions.js";
+import './style.css';
+import Players from './functions.js';
 
-const btnRefresh = document.querySelector("#refresh");
-btnRefresh.addEventListener("click", Players.getPlayer);
+const btnRefresh = document.querySelector('#refresh');
+btnRefresh.addEventListener('click', Players.getPlayer);
 
-const btnSubmit = document.querySelector("#btn-submit");
-btnSubmit.addEventListener("click", (e) => {
+const btnSubmit = document.querySelector('#btn-submit');
+btnSubmit.addEventListener('click', (e) => {
   e.preventDefault();
   Players.addNewPlayer();
 });
