199940d8a9b676b13afcd0a89a8ca770f73747e3
fherrerao <fherrera0206@gmail.com>
2022-01-26 23:40:42 -0500

    Create new player and show it

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import "./style.css";
+import Players from "./functions.js";
 
-const url =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
+Players.createNewGame();
 
-const createNewGame = async () => {
-  const response = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify({
-      name: "Fernando's game",
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  });
+const btnRefresh = document.querySelector("#refresh");
+btnRefresh.addEventListener("click", Players.getPlayer);
 
-  const data = await response.json();
-  console.log(data.result.slice(14, 34));
-};
-
-createNewGame();
+const btnSubmit = document.querySelector("#btn-submit");
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  Players.addNewPlayer();
+});
